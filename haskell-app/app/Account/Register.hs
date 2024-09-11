005c7f3e49a846ea284b1cb7a776450ccecadf5c
saihuff <fumi1take2.s4@gmail.com>
2024-09-12 08:51:31 +0900

    Register implemented

--- haskell-app/app/Account/Register.hs
+++ haskell-app/app/Account/Register.hs
@@ -1,1 +1,29 @@
+{-# LANGUAGE OverloadedStrings #-}
+{-# LANGUAGE DeriveGeneric #-}
+{-# LANGUAGE TypeFamilies #-}
+
 module Account.Register where
+
+import Data.Aeson
+import Data.IORef
+import Control.Monad.IO.Class
+import Database.PostgreSQL.Simple
+import GHC.Generics (Generic)
+
+--User型の定義
+data User = User
+    { userName :: String
+    , userPassword :: String
+    } deriving (Show, Generic)
+
+instance FromJSON User
+instance ToJSON User
+instance FromRow User
+instance ToRow User
+
+-- 新規ユーザ登録エンドポイント
+registerUser :: Connection -> User -> IO ()
+registerUser conn (User name password) = do -- registerUser アプリの状態 ユーザ情報
+    let query = "INSERT INTO users (name, password) VALUES (?, ?)" -- クエリを定義
+    execute conn query (name, password) -- クエリに沿ってデータベース操作
+    return ()
--- haskell-app/app/Main.hs
+++ haskell-app/app/Main.hs
@@ -8,21 +8,34 @@ import Control.Monad.Trans
 import Data.Monoid
 import Data.IORef
 import qualified Data.Text as T
+import Database.PostgreSQL.Simple
+import Data.Aeson
+
+import Account.Register
 
 data MySession = EmptySession
 data MyAppState = DummyAppState (IORef Int)
 
+data AppState = AppState { dbConn :: Connection }
+
 main :: IO ()
 main =
     do ref <- newIORef 0
-       spockCfg <- defaultSpockCfg EmptySession PCNoDatabase (DummyAppState ref)
+       conn <- connectPostgreSQL "host=localhost dbname=postgres user=postgres password=fumifumiHaskell"
+       let appState = AppState conn
+       spockCfg <- defaultSpockCfg EmptySession PCNoDatabase appState
        runSpock 8080 (spock spockCfg app)
 
-app :: SpockM () MySession MyAppState ()
+app :: SpockM () MySession AppState ()
 app =
     do get root $
            text "Hello World!"
-       get ("hello" <//> var) $ \name ->
-           do (DummyAppState ref) <- getState
-              visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
-              text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
+--       get ("hello" <//> var) $ \name ->
+--           do (DummyAppState ref) <- getState
+--              visitorNumber <- liftIO $ atomicModifyIORef' ref $ \i -> (i+1, i+1)
+--              text ("Hello " <> name <> ", you are visitor number " <> T.pack (show visitorNumber))
+       post "/api/register" $ do
+           user <- jsonBody'
+           state <- getState 
+           liftIO $ registerUser (dbConn state) user
+           Web.Spock.json ("User registerd" :: String)
