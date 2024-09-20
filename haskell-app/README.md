e09386ddb4aada95a0aff036cda3f6f1faf47897
saihuff <fumi1take2.s4@gmail.com>
2024-09-20 09:03:31 +0900

    changed

--- haskell-app/README.md
+++ haskell-app/README.md
@@ -1,1 +1,47 @@
 # haskell-app
+
+# URL
+
+## POST ~/api/register
+Content-Type: application/json
+
+{"userName": "<ここにユーザーネーム>", "userPassword": "<ここにパスワード>"}
+
+response -> "User Registerd"
+
+## POST ~/api/login
+Content-Type: application/json
+
+{"userName": "<ここにユーザーネーム>", "userPassword": "<ここにパスワード>"}
+
+response | 成功 -> セッション記録, "Login Successfull"
+         | 失敗 -> "Login Failed"
+
+## GET ~/api/logout
+response -> セッション破棄, "LogOut Success"
+
+## GET ~/api/userdata
+response -> {"userDataID":<ユーザーID(整数型)>,"userDataName":"<ユーザーネーム>","userDataPassword":"<パスワード>","userDataScore":<スコア>,"userDataStudyTime":<総勉強時間>}
+
+## POST ~/api/picture
+Content-Type: application/json
+
+{"getPicture": "<ここにエンコードした画像データ>"}
+
+response -> {"status": "awake"} or {"status": "asleep"}
+
+## GET ~/api/friendcode
+response -> <フレンドコード>
+
+## GET ~/api/registerfriend
+Content-Type: application/json
+
+{"friendID": <フレンドコード>}
+
+response -> 成功すれば "friend registerd"
+
+## GET ~/api/friends
+response -> ["<フレンド1>", "<フレンド2>", ... , "<フレンドn>"]
+
+## GET ~api/getfriendscore
+response -> [["<フレンド1のユーザーネーム>", <フレンド1のスコア>], ["<フレンド2>", <フレンド2のスコア>], ... , ["<フレンドn>", <フレンドnのスコア>]]
--- haskell-app/app/DB/DBconnection.hs
+++ haskell-app/app/DB/DBconnection.hs
@@ -10,6 +10,8 @@ import Control.Monad.IO.Class
 import Database.PostgreSQL.Simple
 import GHC.Generics (Generic)
 import qualified Data.Text as T
+import Data.Text.Read (decimal)
+import Data.Either (rights)
 
 import Account.UserTypes
 
@@ -23,7 +25,7 @@ getFriendCode conn userid = do
 registerFriend :: Connection -> Int -> Int -> IO ()
 registerFriend conn userid friendid = do
     let q = "UPDATE friends SET friend = array_append(friend, ?) WHERE user_id = ?"
-    execute conn q (userid, friendid)
+    execute conn q (friendid, userid)
     return ()
 
 --------------------------------------------------------------------------
@@ -42,22 +44,34 @@ getFriendName conn userid = do
 
 --------------------------------------------------------------------------
 
-getFriendScore :: Connection -> Int -> IO [Int]
+getFriendScore :: Connection -> Int -> IO [(String, Int)]
 getFriendScore conn userid = do
     friendList <- getAllFriends conn userid
     let friends = [getFriendsScore conn id | id <- friendList]
     sequence friends
 
-getFriendsScore :: Connection -> Int -> IO Int
+getFriendsScore :: Connection -> Int -> IO (String, Int)
 getFriendsScore conn userid = do
-    let q = "SELECT score FROM users WHERE id = ?"
-    [result] <- query conn q (Only userid) :: IO [Only Int]
-    return $ fromOnly result
+    let q = "SELECT name, score FROM users WHERE id = ?"
+    [result] <- query conn q (Only userid) :: IO [(String, Int)]
+    return result
 
 --------------------------------------------------------------------------
 
+--getAllFriends :: Connection -> Int -> IO [Int]
+--getAllFriends conn userid = do
+--    let q = "SELECT friend FROM friends WHERE user_id = ?"
+--    [result] <- query conn q (Only userid) :: IO [Only [Int]]
+--    return $ fromOnly result
+
 getAllFriends :: Connection -> Int -> IO [Int]
 getAllFriends conn userid = do
-    let q = "SELECT friend FROM friends WHERE user_id = ?"
-    [result] <- query conn q (Only userid) :: IO [[Int]]
-    return $ result
+    let q = "SELECT array_to_string(friend, ',') FROM friends WHERE user_id = ?"
+    [Only result] <- query conn q (Only userid) :: IO [Only T.Text]
+    -- Split the result by commas and convert to Int
+    let friendIds = map (fst . either error id . decimal) (T.splitOn "," result)
+    return friendIds
+
+quicksort :: Ord a => [a] -> [a]
+quicksort [] = []
+quicksort (x:xs) = quicksort [y | y <- xs, y <= x] ++ [x] ++ [y | y <- xs, y > x]
